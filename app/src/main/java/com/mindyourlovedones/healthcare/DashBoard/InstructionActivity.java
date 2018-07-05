5a974cb8801d14b02aba845974df0a11fa19b833
Nikita <nikita.kurliye@quagnitia.com>
2018-07-05 15:26:24 +0530

    notes, appointment, medical, personal, dropbox, living, card ftu changes done - Nikita

--- app/src/main/java/com/mindyourlovedones/healthcare/DashBoard/InstructionActivity.java
+++ app/src/main/java/com/mindyourlovedones/healthcare/DashBoard/InstructionActivity.java
@@ -28,6 +28,20 @@ public class InstructionActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        //nikita
+        final RelativeLayout relMsg = findViewById(R.id.relMsg);
+        TextView txt61 = findViewById(R.id.txtPolicy61);
+        TextView txt62 = findViewById(R.id.txtPolicy62);
+        TextView txt63 = findViewById(R.id.txtPolicy63);
+        TextView txt64 = findViewById(R.id.txtPolicy64);
+        TextView txt65 = findViewById(R.id.txtPolicy65);
+
+        ImageView img61 = findViewById(R.id.img61);
+        ImageView img62 = findViewById(R.id.img62);
+        ImageView img63 = findViewById(R.id.img63);
+        ImageView img64 = findViewById(R.id.img64);
+        ImageView img65 = findViewById(R.id.img65);
+
         header = findViewById(R.id.header);
         imgBack = findViewById(R.id.imgBack);
         imgPicture = findViewById(R.id.imgPicture);
@@ -42,11 +56,42 @@ public class InstructionActivity extends AppCompatActivity {
             case "Dropbox":
                 imgPicture.setVisibility(View.GONE);
                 imgDot.setVisibility(View.VISIBLE);
-                String msg = "This is a method for personal cloud storage. It is commonly referred to as a method to backup one's files online. In addition to store the files, one can also share them with colleagues and friends, all using Dropbox. To store, retrieve and manage your files, one can use a Dropbox application." +
-                        "<br><br>" +
-                        "You need to login with your dropbox account. If you don't  have dropbox account, You need to have one. " +
-                        "<br><br>" +
-                        "<b>Guidelines:</b>" +
+//                String msg = "This is a method for personal cloud storage. It is commonly referred to as a method to backup one's files online. In addition to store the files, one can also share them with colleagues and friends, all using Dropbox. To store, retrieve and manage your files, one can use a Dropbox application." +
+//                        "<br><br>" +
+//                        "You need to login with your dropbox account. If you don't  have dropbox account, You need to have one. " +
+//                        "<br><br>" +
+//                        "<b>Guidelines:</b>" +
+//                        "<br><br>" +
+//                        "<b>1) Login: </b>" +
+//                        "<br>" +
+//                        "Click on <b>\"Login With DropBox\" </b>button, It will redirect you to dropbox site. Now you can login with your personal dropbox account." +
+//                        "<br>" +
+//                        "After logging in, To allow the access to the files and folders from your dropbox, Click the <b>'Allow'</b>  Button.So now you can Access your files." +
+//                        "<br><br>" +
+//                        "<b>2) Backup: </b>" +
+//                        "<br>" +
+//                        "If you have logged in to dropbox from application, It will show you Backup Options." +
+//                        "<br>" +
+//                        "On clicking  <b>'BACKUP'</b> button, you need to Click on <b>'Backup your Data'</b> button to store your profile." +
+//                        "<br><br>" +
+//                        "<b>3) Restore: </b>" +
+//                        "<br>" +
+//                        "If you have logged in to dropbox from application, It will show you Restore Options. It will help to restore profiles." +
+//                        "<br>" +
+//                        "On clicking  <b>'RESTORE'</b>  button, you will get list of zip files for download. On clicking any of the zip file, It will be downloaded and you will get a popup message  to unzip and restore file." +
+//                        "<br><br>" +
+//                        "<b>4) Get Files: </b>" +
+//                        "<br>" +
+//                        "If you have logged in to dropbox from application, It will show you View Files Options. It will help you to get the documents in Advance Directive section  from dropbox storage." +
+//                        "<br>" +
+//                        "On clicking <b>'VIEW FILES'</b> button, you will get list of documents files for download. On clicking any of the document  file, It will be downloaded ." +
+//                        "<br>" +
+//                        "Now you can add this document  to your application by clicking on <b>'ADD FILES'</b> button.";
+//
+//                txtMsg.setText(Html.fromHtml(msg));
+
+                //nikita
+                String msgg = "<b>Guidelines:</b>" +
                         "<br><br>" +
                         "<b>1) Login: </b>" +
                         "<br>" +
@@ -73,8 +118,25 @@ public class InstructionActivity extends AppCompatActivity {
                         "On clicking <b>'VIEW FILES'</b> button, you will get list of documents files for download. On clicking any of the document  file, It will be downloaded ." +
                         "<br>" +
                         "Now you can add this document  to your application by clicking on <b>'ADD FILES'</b> button.";
+                //nikita
+                txt61.setText(Html.fromHtml("This is a method for personal cloud storage. It is commonly referred to as a method to backup one's files online. In addition to store the files, one can also share them with colleagues and friends, all using Dropbox. To store, retrieve and manage your files, one can use a Dropbox application."));
+                txt62.setText(Html.fromHtml("You need to login with your dropbox account. If you don't  have dropbox account, You need to have one. "));
+                txt63.setText(Html.fromHtml(msgg));
 
-                txtMsg.setText(Html.fromHtml(msg));
+                //nikita
+                txt61.setVisibility(View.VISIBLE);
+                txt62.setVisibility(View.VISIBLE);
+                txt63.setVisibility(View.VISIBLE);
+                txt64.setVisibility(View.GONE);
+                txt65.setVisibility(View.GONE);
+
+                //nikita
+                img61.setVisibility(View.VISIBLE);
+                img62.setVisibility(View.VISIBLE);
+                img63.setVisibility(View.VISIBLE);
+                img64.setVisibility(View.GONE);
+                img65.setVisibility(View.GONE);
+
                 txtHeader.setText("Dropbox");
                 break;
 
@@ -82,14 +144,35 @@ public class InstructionActivity extends AppCompatActivity {
                 imgPicture.setVisibility(View.GONE);
                 imgDot.setVisibility(View.GONE);
                 imgPicture.setImageResource(R.drawable.v_user);
-                String msgs = "To <b>add</b> information type responses.<br>" +
-                        "To <b>save</b> information click the check mark" +
-                        " on the <b>top right</b> side of the screen.<br><br>" +
-                        "To <b>edit</b> or <b>delete</b> information simply work on the screen and then save your edits by clicking on the <b>check mark</b> on the <b>top right</b> side of the screen." +
-                        "<br><br>" +
-                        "To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the <b>three dots</b> on the top right side of the screen.";
+//                String msgs = "To <b>add</b> information type responses.<br>" +
+//                        "To <b>save</b> information click the check mark" +
+//                        " on the <b>top right</b> side of the screen.<br><br>" +
+//                        "To <b>edit</b> or <b>delete</b> information simply work on the screen and then save your edits by clicking on the <b>check mark</b> on the <b>top right</b> side of the screen." +
+//                        "<br><br>" +
+//                        "To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the <b>three dots</b> on the top right side of the screen.";
+//
+//                txtMsg.setText(Html.fromHtml(msgs));
 
-                txtMsg.setText(Html.fromHtml(msgs));
+                //nikita
+                txt61.setText(Html.fromHtml("To <b>add</b> information type responses."));
+                txt62.setText(Html.fromHtml("To <b>save</b> information click the check mark on the <b>top right</b> side of the screen."));
+                txt63.setText(Html.fromHtml("To <b>edit</b> or <b>delete</b> information simply work on the screen and then save your edits by clicking on the <b>check mark</b> on the <b>top right</b> side of the screen."));
+                txt64.setText(Html.fromHtml("To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the <b>three dots</b> on the top right side of the screen."));
+
+                //nikita
+                txt61.setVisibility(View.VISIBLE);
+                txt62.setVisibility(View.VISIBLE);
+                txt63.setVisibility(View.VISIBLE);
+                txt64.setVisibility(View.VISIBLE);
+                txt65.setVisibility(View.GONE);
+
+                //nikita
+                img61.setVisibility(View.VISIBLE);
+                img62.setVisibility(View.VISIBLE);
+                img63.setVisibility(View.VISIBLE);
+                img64.setVisibility(View.VISIBLE);
+                img65.setVisibility(View.GONE);
+
                 txtHeader.setText("Personal Profile Instructions");
                 break;
 
@@ -97,19 +180,41 @@ public class InstructionActivity extends AppCompatActivity {
                 imgPicture.setVisibility(View.GONE);
                 imgDot.setVisibility(View.GONE);
                 imgPicture.setImageResource(R.drawable.v_user);
-                String msgd = "To <b>add</b> information click the <b>plus box</b> for each section " +
-                        "and then click the green bar." +
-                        "<br><br>" +
-                        "To <b>save</b> information click the <b>check mark</b>" +
-                        " on the top right side of the screen." +
-                        "<br><br>" +
-                        "To <b>edit</b> information in a particular section click the picture of the <b>pencil</b>. To save your edits click the green bar at the lower half of the screen." +
-                        "<br><br>" +
-                        "To <b>delete</b> information in a particular section click the <b>garbage can</b>. For sections without the garbage can, simply delete the data." +
-                        "<br><br>" +
-                        "To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the three dots on the top right side of the screen.";
+//                String msgd = "To <b>add</b> information click the <b>plus box</b> for each section " +
+//                        "and then click the green bar." +
+//                        "<br><br>" +
+//                        "To <b>save</b> information click the <b>check mark</b>" +
+//                        " on the top right side of the screen." +
+//                        "<br><br>" +
+//                        "To <b>edit</b> information in a particular section click the picture of the <b>pencil</b>. To save your edits click the green bar at the lower half of the screen." +
+//                        "<br><br>" +
+//                        "To <b>delete</b> information in a particular section click the <b>garbage can</b>. For sections without the garbage can, simply delete the data." +
+//                        "<br><br>" +
+//                        "To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the three dots on the top right side of the screen.";
+//
+//                txtMsg.setText(Html.fromHtml(msgd));
 
-                txtMsg.setText(Html.fromHtml(msgd));
+                //nikita
+                txt61.setText(Html.fromHtml("To <b>add</b> information click the <b>plus box</b> for each section and then click the green bar."));
+                txt62.setText(Html.fromHtml("To <b>save</b> information click the <b>check mark</b> on the top right side of the screen."));
+                txt63.setText(Html.fromHtml("To <b>edit</b> information in a particular section click the picture of the <b>pencil</b>. To save your edits click the green bar at the lower half of the screen."));
+                txt64.setText(Html.fromHtml("To <b>delete</b> information in a particular section click the <b>garbage can</b>. For sections without the garbage can, simply delete the data."));
+                txt65.setText(Html.fromHtml("To <b>view a report</b> or to <b>email</b> or <b>fax</b> the data in each section click the three dots on the top right side of the screen."));
+
+                //nikita
+                txt61.setVisibility(View.VISIBLE);
+                txt62.setVisibility(View.VISIBLE);
+                txt63.setVisibility(View.VISIBLE);
+                txt64.setVisibility(View.VISIBLE);
+                txt65.setVisibility(View.VISIBLE);
+
+                //nikita
+                img61.setVisibility(View.VISIBLE);
+                img62.setVisibility(View.VISIBLE);
+                img63.setVisibility(View.VISIBLE);
+                img64.setVisibility(View.VISIBLE);
+                img65.setVisibility(View.VISIBLE);
+
                 txtHeader.setText("Medical Profile Instructions");
                 break;
 
@@ -117,14 +222,34 @@ public class InstructionActivity extends AppCompatActivity {
                 imgPicture.setVisibility(View.GONE);
                 imgDot.setVisibility(View.GONE);
                 imgPicture.setImageResource(R.drawable.v_user);
-                String msgl = "To <b>save</b> information click the check mark" +
-                        " on the top right side of the screen." +
-                        "<br><br>" +
-                        "To <b>edit</b> information simply change the data and then save your edits by clicking on the check mark on the top right side of the screen." +
-                        "<br><br>" +
-                        "To <b>view</b>, <b>email</b>, or <b>fax</b> the data in each section click on the three dots on the top right side of the screen.";
+//                String msgl = "To <b>save</b> information click the check mark" +
+//                        " on the top right side of the screen." +
+//                        "<br><br>" +
+//                        "To <b>edit</b> information simply change the data and then save your edits by clicking on the check mark on the top right side of the screen." +
+//                        "<br><br>" +
+//                        "To <b>view</b>, <b>email</b>, or <b>fax</b> the data in each section click on the three dots on the top right side of the screen.";
+//
+//                txtMsg.setText(Html.fromHtml(msgl));
 
-                txtMsg.setText(Html.fromHtml(msgl));
+                //nikita
+                txt61.setText(Html.fromHtml("To <b>save</b> information click the check mark on the top right side of the screen."));
+                txt62.setText(Html.fromHtml("To <b>edit</b> information simply change the data and then save your edits by clicking on the check mark on the top right side of the screen."));
+                txt63.setText(Html.fromHtml("To <b>view</b>, <b>email</b>, or <b>fax</b> the data in each section click on the three dots on the top right side of the screen."));
+
+                //nikita
+                txt61.setVisibility(View.VISIBLE);
+                txt62.setVisibility(View.VISIBLE);
+                txt63.setVisibility(View.VISIBLE);
+                txt64.setVisibility(View.GONE);
+                txt65.setVisibility(View.GONE);
+
+                //nikita
+                img61.setVisibility(View.VISIBLE);
+                img62.setVisibility(View.VISIBLE);
+                img63.setVisibility(View.VISIBLE);
+                img64.setVisibility(View.GONE);
+                img65.setVisibility(View.GONE);
+
                 txtHeader.setText("Living Activity Instructions");
                 break;
 
@@ -141,6 +266,27 @@ public class InstructionActivity extends AppCompatActivity {
                         "To <b>view</b> the card <b>click</b> on the <b>card</b>, So you can <b>view</b> card and see by <b>zooming</b> it. To <b>share card</b> to your loved ones click the <b>share</b> icon on the top right side of the screen.";
 
                 txtMsg.setText(Html.fromHtml(msgc));
+
+                //nikita
+                txt61.setText(Html.fromHtml("You can <b>add</b> the card from both the sides i.e <b>front and back</b>."));
+                txt62.setText(Html.fromHtml("For adding front and back card, You need to click on the <b>plus</b> icon. Now you can add picture of card either from <b>Camera or Gallery</b>."));
+                txt63.setText(Html.fromHtml("For adding card through <b>camera</b>, It is <b>recommended</b> that you hold your phone <b>horizontal</b> while taking a picture of a card."));
+                txt64.setText(Html.fromHtml("To <b>view</b> the card <b>click</b> on the <b>card</b>, So you can <b>view</b> card and see by <b>zooming</b> it. To <b>share card</b> to your loved ones click the <b>share</b> icon on the top right side of the screen."));
+
+                //nikita
+                txt61.setVisibility(View.VISIBLE);
+                txt62.setVisibility(View.VISIBLE);
+                txt63.setVisibility(View.VISIBLE);
+                txt64.setVisibility(View.VISIBLE);
+                txt65.setVisibility(View.GONE);
+
+                //nikita
+                img61.setVisibility(View.VISIBLE);
+                img62.setVisibility(View.VISIBLE);
+                img63.setVisibility(View.VISIBLE);
+                img64.setVisibility(View.VISIBLE);
+                img65.setVisibility(View.GONE);
+
                 txtHeader.setText("Insurance Card GuideLines");
                 break;
 
