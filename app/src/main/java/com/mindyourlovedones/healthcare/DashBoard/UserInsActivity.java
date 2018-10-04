78ee3e7e2f8454509fd74030af128eca26454d9d
Nikita <nikita.kurliye@quagnitia.com>
2018-10-04 19:06:39 +0530

    Changes for email option  - Nikita

--- app/src/main/java/com/mindyourlovedones/healthcare/DashBoard/UserInsActivity.java
+++ app/src/main/java/com/mindyourlovedones/healthcare/DashBoard/UserInsActivity.java
@@ -16,11 +16,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mindyourlovedones.healthcare.HomeActivity.R;
+import com.mindyourlovedones.healthcare.utility.UIEmails;
 
 public class UserInsActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView imgBack;
     TextView txtEmail, txtStep1, txtStep2, txtStep22, txtStep3, txtStep4, txtStep5, txtStep55, txtStep555, txtStep6, txtStep7, txtStep8;
     Context context = this;
+
+    String UI="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,7 @@ public class UserInsActivity extends AppCompatActivity implements View.OnClickLi
         txtStep8 = findViewById(R.id.txtStep8);
 
 
+
         txtStep1.setText(Html.fromHtml("<b>Welcome to MYLO</b> –  This App allows you to maintain a digital library of critical documents and medical information for you and your loved ones. There are 6 main sections. The App provides unlimited profiles, pdf reports, email and fax functionality, and the ability to share profiles and eliminate the need to re-enter data. \n\n"));
         txtStep2.setText(Html.fromHtml("<b>Getting Started</b> - Before you begin we would like to explain the data entry process. Except for this Profiles page, each screen has a top bar which includes the name of the screen, an arrow back button on the top left, three dots on the top right (which allows you to view or email the data), and a save button on data entry screens. \n\n"));
         txtStep22.setText(Html.fromHtml("\n\nFirst time Users will see First Time User Instructions directly on the screen. After adding information. the User can access User Instructions by clicking on the three buttons. A User Guide is also available from the dropdown Menu located on the top left of the Profiles page. \n\n"));
@@ -62,6 +66,10 @@ public class UserInsActivity extends AppCompatActivity implements View.OnClickLi
         txtStep6.setText(Html.fromHtml("There are <b>three</b> required elements to create a Profile –  <b>name, relationship</b>, and an <b>email</b> address.\n\n"));
         txtStep7.setText(Html.fromHtml("<b>To Share a Profile –</b> Long press on the Profile Box and you will be able to upload the Profile to your Dropbox account.\n\n"));
         txtStep8.setText(Html.fromHtml("<b>To Delete a Profile –</b> Long Press on the Profile Box.\n\nThanks & Regards"));
+
+
+        UI=Html.fromHtml("<b>Welcome to MYLO</b> –  This App allows you to maintain a digital library of critical documents and medical information for you and your loved ones. There are 6 main sections. The App provides unlimited profiles, pdf reports, email and fax functionality, and the ability to share profiles and eliminate the need to re-enter data. \n\n <b>Getting Started</b> - Before you begin we would like to explain the data entry process. Except for this Profiles page, each screen has a top bar which includes the name of the screen, an arrow back button on the top left, three dots on the top right (which allows you to view or email the data), and a save button on data entry screens. \n\n\n\nFirst time Users will see First Time User Instructions directly on the screen. After adding information. the User can access User Instructions by clicking on the three buttons. A User Guide is also available from the dropdown Menu located on the top left of the Profiles page. \n\n <b>A reminder to all Users</b> - MYLO is a native to your phone. This means that we do not have access to your information. If you forget the name and email address used for this app then the data will be lost. It’s important that you  remember this information and to periodically back up the and or send yourself the pdf reports. Backup instructions are included on the Menu page.\n\n <b>MYLO Would Like to Access Your Contacts and MYLO Would Like to Access Your Camera</b> - This message does NOT mean that the company has access to your information, it simply allows the APP to utilise the functions of your phone and provide the USER with a better experience. \n\n <b>Adding a Profile</b> - click the plus box. You will see two options. Create New and Import from Dropbox. \n\n <b>Option 1</b> : Create New. You will be brought to the Add Profile  Screen. You can type in the new profile or if the person is in your Contacts then click the gray bar on the top right side of your screen to load information. Once completed click either Save (top right of screen) or Add Profile (bottom of screen).\n\n <b>Option 2</b> : Import from Dropbox. Using this feature you can upload a profile from Dropbox. If you click that option, you will be provided additional instructions. Once completed click either Save (top right of screen) or Add Profile (bottom of screen). \n\n There are <b>three</b> required elements to create a Profile –  <b>name, relationship</b>, and an <b>email</b> address.\n\n <b>To Share a Profile –</b> Long press on the Profile Box and you will be able to upload the Profile to your Dropbox account.\n\n <b>To Delete a Profile –</b> Long Press on the Profile Box.\n\nThanks & Regards").toString();
+
 
     }
 
@@ -104,7 +112,9 @@ public class UserInsActivity extends AppCompatActivity implements View.OnClickLi
         txtYes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "Still in progress please wait..!!", Toast.LENGTH_SHORT).show();
+                UIEmails ui = new UIEmails();
+                ui.emailAttachement(UserInsActivity.this,"MYLO User Instructions",UI);
+//                Toast.makeText(context, "Still in progress please wait..!!", Toast.LENGTH_SHORT).show();
                 dialogEmailIns.dismiss();
             }
         });
