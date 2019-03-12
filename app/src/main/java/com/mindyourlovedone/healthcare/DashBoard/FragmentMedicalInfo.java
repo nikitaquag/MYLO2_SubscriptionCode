b49844fe2c055a430764c1074a2f437f5d334c43
Nikita <nikita.kurliye@quagnitia.com>
2019-03-12 17:23:34 +0530

    Medical Profile changes - in progress

--- app/src/main/java/com/mindyourlovedone/healthcare/DashBoard/FragmentMedicalInfo.java
+++ app/src/main/java/com/mindyourlovedone/healthcare/DashBoard/FragmentMedicalInfo.java
@@ -34,6 +34,7 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.draw.LineSeparator;
+import com.mindyourlovedone.healthcare.Activity.TransparentActivity;
 import com.mindyourlovedone.healthcare.Connections.MedAdapter;
 import com.mindyourlovedone.healthcare.Connections.RelationActivity;
 import com.mindyourlovedone.healthcare.HomeActivity.BaseActivity;
@@ -111,7 +112,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
     String[] BloodList = {"", "A - negative", "A - positive", "AB - negative", "AB - positive", "B - negative", "B - positive", "O - negative", "O - positive", "I don't know"};
     Preferences preferences;
     DBHelper dbHelper;
-    FloatingActionButton floatProfile;
+    FloatingActionButton floatProfile,floatOptions;
 
     TextView txtBlood;
     ImageView imgBloodDrop;
@@ -146,6 +147,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
 
     private void initListener() {
         floatProfile.setOnClickListener(this);
+        floatOptions.setOnClickListener(this);
         txtSave.setOnClickListener(this);
 
         imgDone.setOnClickListener(this);
@@ -238,6 +240,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
         txtBlood.setFocusable(false);
 
         floatProfile = rootview.findViewById(R.id.floatProfile);
+        floatOptions = rootview.findViewById(R.id.floatProfile);
         imgInfo = rootview.findViewById(R.id.imgInfo);
         imgInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -683,6 +686,9 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
         final ArrayList allergyList = new ArrayList();
         ImplantsLists = MedicalImplantsQuery.fetchAllRecord(preferences.getInt(PrefConstants.CONNECTED_USERID));
         if (ImplantsLists.size() != 0) {
+            if(flagImplants) {
+                llSubImplants.setVisibility(View.VISIBLE);
+            }
             ListImplants.setVisibility(View.VISIBLE);
             for (int i = 0; i < ImplantsLists.size(); i++) {
                 Implant a = ImplantsLists.get(i);
@@ -703,6 +709,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
                 ListImplants.setVisibility(View.VISIBLE);
             }
         } else {
+            llSubImplants.setVisibility(View.GONE);
             ListImplants.setVisibility(View.GONE);
         }
     }
@@ -736,6 +743,9 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
         final ArrayList allergyList = new ArrayList();
         HistoryLists = HistoryQuery.fetchHistoryRecord(preferences.getInt(PrefConstants.CONNECTED_USERID));
         if (HistoryLists.size() != 0) {
+            if(flagHistory) {
+                llSubHistory.setVisibility(View.VISIBLE);
+            }
             ListHistory.setVisibility(View.VISIBLE);
             for (int i = 0; i < HistoryLists.size(); i++) {
                 History a = HistoryLists.get(i);
@@ -755,6 +765,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
             }
         } else {
             ListHistory.setVisibility(View.GONE);
+            llSubHistory.setVisibility(View.GONE);
         }
     }
 
@@ -785,12 +796,15 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
     private void setHospitalData() {
         HospitalList = HospitalQuery.fetchAllRecord(preferences.getInt(PrefConstants.CONNECTED_USERID));
         if (HospitalList.size() != 0) {
-            //shradha
+            if(flagHistory) {
+                llSubHistory.setVisibility(View.VISIBLE);
+            }
             MedAdapter md = new MedAdapter(getActivity(), HospitalList, "hospital", FragmentMedicalInfo.this);
             ListHospital.setAdapter(md);
             ListHospital.setVisibility(View.VISIBLE);
         } else {
             ListHospital.setVisibility(View.GONE);
+            llSubHospital.setVisibility(View.GONE);
         }
     }
 
@@ -822,6 +836,9 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
         final ArrayList allergyList = new ArrayList();
         VaccineLists = VaccineQuery.fetchAllRecord(preferences.getInt(PrefConstants.CONNECTED_USERID));
         if (VaccineLists.size() != 0) {
+            if(flagVaccine) {
+                linVaccine.setVisibility(View.VISIBLE);
+            }
             ListVaccine.setVisibility(View.VISIBLE);
             for (int i = 0; i < VaccineLists.size(); i++) {
                 Vaccine a = VaccineLists.get(i);
@@ -840,6 +857,7 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
                 ListVaccine.setVisibility(View.VISIBLE);
             }
         } else {
+            linVaccine.setVisibility(View.GONE);
             ListVaccine.setVisibility(View.GONE);
         }
     }
@@ -1105,10 +1123,16 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
                 break;
             case R.id.imgVaccineDrop:
                 if (flagVaccine == false) {
-                    linVaccine.setVisibility(View.VISIBLE);
+                    if(!VaccineLists.isEmpty()){
+                        linVaccine.setVisibility(View.VISIBLE);
+                    }else{
+                        linVaccine.setVisibility(View.GONE);
+                    }
+                    txtAddVaccine.setVisibility(View.VISIBLE);
                     imgVaccineDrop.setImageResource(R.drawable.dropup);
                     flagVaccine = true;
                 } else if (flagVaccine == true) {
+                    txtAddVaccine.setVisibility(View.GONE);
                     linVaccine.setVisibility(View.GONE);
                     imgVaccineDrop.setImageResource(R.drawable.drop_down);
                     flagVaccine = false;
@@ -1184,7 +1208,11 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
 
             case R.id.imgAddImplants:
                 if (flagImplants == false) {
-                    llSubImplants.setVisibility(View.VISIBLE);
+                    if(!ImplantsLists.isEmpty()){
+                        llSubImplants.setVisibility(View.VISIBLE);
+                    }else{
+                        llSubImplants.setVisibility(View.GONE);
+                    }
                     imgAddImplants.setImageResource(R.drawable.dropup);
                     txtAddImplants.setVisibility(View.VISIBLE);
 //                    viewImplantsBottom.setVisibility(View.GONE);
@@ -1200,7 +1228,11 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
 
             case R.id.imgAddHistory:
                 if (flagHistory == false) {
-                    llSubHistory.setVisibility(View.VISIBLE);
+                    if(!HistoryLists.isEmpty()){
+                        llSubHistory.setVisibility(View.VISIBLE);
+                    }else{
+                        llSubHistory.setVisibility(View.GONE);
+                    }
                     imgAddHistory.setImageResource(R.drawable.dropup);
                     txtAddHistory.setVisibility(View.VISIBLE);
 //                    viewHistoryBottom.setVisibility(View.GONE);
@@ -1215,7 +1247,11 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
                 break;
             case R.id.imgAddHospital:
                 if (flagHospital == false) {
-                    llSubHospital.setVisibility(View.VISIBLE);
+                    if(!HospitalList.isEmpty()){
+                        llSubHospital.setVisibility(View.VISIBLE);
+                    }else{
+                        llSubHospital.setVisibility(View.GONE);
+                    }
                     imgAddHospital.setImageResource(R.drawable.dropup);
                     txtAddHospital.setVisibility(View.VISIBLE);
 //                    viewHospitalBottom.setVisibility(View.GONE);
@@ -1303,6 +1339,11 @@ public class FragmentMedicalInfo extends Fragment implements View.OnClickListene
                 //  intentDashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 //  intentDashboard.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intentDashboard);
+                break;
+
+            case R.id.floatOptions:
+                Intent intentfloat = new Intent(getActivity(), TransparentActivity.class);
+                startActivity(intentfloat);
                 break;
 
             case R.id.imgBack:
